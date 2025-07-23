61f23785e61ffee4ef17052d9164a1d55ffdbc5c
WeAreDevs-ops <cleencuevas@gmail.com>
2025-07-24 07:50:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ async function isBotInGuild(guildId) {
 
 function generateBotInviteLink(guildId) {
   const clientId = process.env.DISCORD_CLIENT_ID;
-  const permissions = '1073820728';
+  const permissions = '75804';
   const scope = 'bot%20applications.commands';
   const redirectUri = encodeURIComponent(DISCORD_REDIRECT_URI);
   
